5aa7cc102937c8656491c54473ceb7e0d9f47f2f
Linguardium <61733861+Linguardium@users.noreply.github.com>
2025-01-21 20:05:51 -0600

    Throwing Knife Entity porting

--- common/src/main/java/com/uraneptus/sullysmod/common/entities/ThrownThrowingKnife.java
+++ common/src/main/java/com/uraneptus/sullysmod/common/entities/ThrownThrowingKnife.java
@@ -4,103 +4,91 @@ import com.uraneptus.sullysmod.core.registry.SMDamageTypes;
 import com.uraneptus.sullysmod.core.registry.SMEntityTypes;
 import com.uraneptus.sullysmod.core.registry.SMItems;
 import com.uraneptus.sullysmod.core.registry.SMSounds;
-import net.minecraft.nbt.CompoundTag;
+import com.uraneptus.sullysmod.mixins.DamageSourcesHelper;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
-import net.minecraftforge.network.PlayMessages;
 
 public class ThrownThrowingKnife extends AbstractArrow {
-    private ItemStack knifeItem = new ItemStack(SMItems.THROWING_KNIFE.get());
+    private static ItemStack knifeItem = new ItemStack(SMItems.THROWING_KNIFE.get());
 
     public ThrownThrowingKnife(EntityType<? extends ThrownThrowingKnife> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
-    }
 
-    public ThrownThrowingKnife(PlayMessages.SpawnEntity spawnEntity, Level level) {
-        this(SMEntityTypes.THROWN_THROWING_KNIFE.get(), level);
     }
 
     public ThrownThrowingKnife(Level pLevel, LivingEntity pShooter, ItemStack pStack) {
-        super(SMEntityTypes.THROWN_THROWING_KNIFE.get(), pShooter, pLevel);
-        if (pShooter instanceof Player) {
-            this.pickup = AbstractArrow.Pickup.ALLOWED;
-        }
+        super(SMEntityTypes.THROWN_THROWING_KNIFE.get(), pShooter, pLevel, pStack, pStack);
+        // Handled by parent class
+//        if (pShooter instanceof Player) {
+//            this.pickup = AbstractArrow.Pickup.ALLOWED;
+//        }
     }
 
     public ThrownThrowingKnife(Level pLevel, double pX, double pY, double pZ) {
-        super(SMEntityTypes.THROWN_THROWING_KNIFE.get(), pX, pY, pZ, pLevel);
+        super(SMEntityTypes.THROWN_THROWING_KNIFE.get(), pX, pY, pZ, pLevel, knifeItem.copy(), knifeItem.copy());
     }
 
     @Override
-    public ItemStack getPickupItem() {
+    protected ItemStack getDefaultPickupItem() {
         return this.knifeItem.copy();
     }
 
     @Override
     protected void onHitEntity(EntityHitResult pResult) {
         Entity entity = pResult.getEntity();
-        float f = 2.0F;
+        if (entity.getType() == EntityType.ENDERMAN) return;
 
-        if (!entity.onGround() && !entity.isInFluidType()) {
-            f += 4.0F;
+        float damageAmount = 2.0F;
+        if (!entity.onGround() && !entity.isUnderWater()) {
+            damageAmount += 4.0F;
         }
 
+        this.playSound(SMSounds.THROWING_KNIFE_HIT.get(), 1.0F, 1.0F);
+
+        if (!(this.level() instanceof ServerLevel serverLevel)) return;
 
         Entity owner = this.getOwner();
-        if (entity.hurt(this.damageSources().source(SMDamageTypes.THROWING_KNIFE, this, owner == null ? this : owner), f)) {
-            if (entity.getType() == EntityType.ENDERMAN) {
-                return;
-            }
+        DamageSource damageSource = ((DamageSourcesHelper)this.damageSources()).createSource(SMDamageTypes.THROWING_KNIFE, this, (owner != null) ? owner : this);
 
-            if (entity instanceof LivingEntity livingEntity) {
-                if (owner instanceof LivingEntity ownerEntity) {
-                    EnchantmentHelper.doPostHurtEffects(livingEntity, ownerEntity);
-                    EnchantmentHelper.doPostDamageEffects(ownerEntity, livingEntity);
-                }
+        ItemStack weaponItem = this.getWeaponItem();
+        if (weaponItem == null || weaponItem.isEmpty()) weaponItem = this.getPickupItem();
 
-                this.doPostHurtEffects(livingEntity);
-            }
-            this.discard();
-        } else {
-            this.setDeltaMovement(this.getDeltaMovement().scale(-0.1D));
-            this.setYRot(this.getYRot() + 180.0F);
-            this.yRotO += 180.0F;
-            if (!this.level().isClientSide && this.getDeltaMovement().lengthSqr() < 1.0E-7D) {
-                if (this.pickup == AbstractArrow.Pickup.ALLOWED) {
-                    this.spawnAtLocation(this.getPickupItem(), 0.1F);
-                }
+        damageAmount = EnchantmentHelper.modifyDamage(serverLevel, weaponItem, entity, damageSource, damageAmount);
+        if (!entity.hurtServer(serverLevel, damageSource, damageAmount)) return;
 
-                this.discard();
-            }
-        }
-        this.playSound(SMSounds.THROWING_KNIFE_HIT.get(), 1.0F, 1.0F);
+        EnchantmentHelper.doPostAttackEffectsWithItemSource(serverLevel, entity, damageSource, this.getWeaponItem());
+        if (entity instanceof LivingEntity target) this.doPostHurtEffects(target);
+
+        this.discard();
+
+        // Handled by deflection in parent class
+
+//        } else {
+//            this.setDeltaMovement(this.getDeltaMovement().scale(-0.1D));
+//            this.setYRot(this.getYRot() + 180.0F);
+//            this.yRotO += 180.0F;
+//            if (this.level() instanceof ServerLevel serverLevel && this.getDeltaMovement().lengthSqr() < 1.0E-7D) {
+//                if (this.pickup == AbstractArrow.Pickup.ALLOWED) {
+//                    this.spawnAtLocation(serverLevel, this.getPickupItem(), 0.1F);
+//                }
+//
+//                this.discard();
+//            }
+//        }
     }
 
     @Override
     protected SoundEvent getDefaultHitGroundSoundEvent() {
         return SMSounds.THROWING_KNIFE_HIT_GROUND.get();
-    }
-
-    @Override
-    public void readAdditionalSaveData(CompoundTag pCompound) {
-        super.readAdditionalSaveData(pCompound);
-        if (pCompound.contains("ThrowingKnife", 10)) {
-            this.knifeItem = ItemStack.of(pCompound.getCompound("ThrowingKnife"));
-        }
-    }
-
-    @Override
-    public void addAdditionalSaveData(CompoundTag pCompound) {
-        super.addAdditionalSaveData(pCompound);
-        pCompound.put("ThrowingKnife", this.knifeItem.save(new CompoundTag()));
     }
 
     @Override
